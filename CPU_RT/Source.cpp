58f3193870a0d8d472f5e4d2e03999ea2683b7c8
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-03 20:07:06 +0100

    comment added, port of large sample code tomorrow.

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -1,6 +1,10 @@
 #include "iostream"
 // initial blank commit
 
+/// This will be an evolving merge of my attempts to understand much of ray tracing by
+/// merging my own understanding, with that of Peter Shirley`s RT in one weekend series,
+/// and much of the ideas in Ray Tracing from the ground Up --- with a  focus on static CPU side
+/// Ray Tracing.
 
 int main()
 {
