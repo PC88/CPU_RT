2e0b577bf024d257efc9cd308726b4472c18e783
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-04 20:46:45 +0100

    standard error check now added.

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -18,6 +18,7 @@ int main()
 
 	for (int j = image_height - 1; j >= 0; --j) 
 	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) 
 		{
 			auto r = double(i) / (image_width - 1);
@@ -31,6 +32,8 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::cerr << "\nDone.\n";
 	
 	return 0;
 }
