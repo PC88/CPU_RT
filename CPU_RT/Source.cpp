bbccb46c2ce99bd7e64d642ddab3675b5c4f5084
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-08 17:55:03 +0100

    before implementaiton of primitve camera - readme and page changes to record changes coming first.

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -1,17 +1,37 @@
 #include "iostream"
 #include "RT1W/vec3.h"
 #include "Vendor/glm/glm.hpp"
+#include "RT1W/ray.h"
 
 /// This will be an evolving merge of my attempts to understand much of ray tracing by
 /// merging my own understanding, with that of Peter Shirley`s RT in one weekend series,
 /// and much of the ideas in Ray Tracing from the ground Up --- with a  focus on static CPU side
 /// Ray Tracing.
 
+color ray_color(const ray& r) 
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t)*color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
 	// Image
-	const int image_width = 256;
-	const int image_height = 256;
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// Camera
+
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
 	// Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
