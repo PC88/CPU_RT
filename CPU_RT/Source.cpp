8e70ad56d3e6261c28b44300be26d825f509dc78
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-09 15:31:28 +0100

    adding a sphere intersection to the ray tracer, and viewing the output

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -8,12 +8,28 @@
 /// and much of the ideas in Ray Tracing from the ground Up --- with a  focus on static CPU side
 /// Ray Tracing.
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a*c;
+	return (discriminant > 0);
+}
+
 color ray_color(const ray& r) 
 {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+	{
+		return color(1, 0, 0);
+	}
+
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0 - t)*color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
+
 
 int main()
 {
