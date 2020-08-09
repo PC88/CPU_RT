84bafae879c139bea13e33215263ce490875dcc1
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-09 15:19:01 +0100

    first ray-trace image added, and output writing line moved and uncommented. - now on section 5 of RTOW

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -42,11 +42,11 @@ int main()
 		for (int i = 0; i < image_width; ++i) 
 		{
 			//color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
-			//write_color(std::cout, pixel_color);
 			auto u = double(i) / (image_width - 1);
 			auto v = double(j) / (image_height - 1);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
 			color pixel_color = ray_color(r);
+			write_color(std::cout, pixel_color);
 		}
 	}
 
