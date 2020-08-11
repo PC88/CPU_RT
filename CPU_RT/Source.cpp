0fc7ab1e70f86777c1ba38fcd50acc8481c27949
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-11 20:18:27 +0100

    main updated to reflect the changes made, simple world definition, ray colour function updated

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -4,6 +4,8 @@
 #include "RT1W/ray.h"
 #include "RT1W/hittable.h"
 #include "RT1W/sphere.h"
+#include "RT1W/hittable_list.h"
+#include "RT1W/rtweekend.h"
 
 /// This will be an evolving merge of my attempts to understand much of ray tracing by
 /// merging my own understanding, with that of Peter Shirley`s RT in one weekend series,
@@ -35,18 +37,16 @@ double hit_sphere(const point3& center, double radius, const ray& r)
 	}
 }
 
-color ray_color(const ray& r) 
+color ray_color(const ray& r, const hittable& world)
 {
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-
-	if (t > 0.0) 
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec)) 
 	{
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5*color(N.x() + 1, N.y() + 1, N.z() + 1);
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5*(unit_direction.y() + 1.0);
+	auto t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0 - t)*color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -57,6 +57,11 @@ int main()
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
 
@@ -81,7 +86,7 @@ int main()
 			auto u = double(i) / (image_width - 1);
 			auto v = double(j) / (image_height - 1);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
