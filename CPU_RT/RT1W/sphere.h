f10eaba1dd9da82d2c7efb3d8e9e91f7507be6ae
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-11 20:07:54 +0100

    modifications to how normals are treated in the hit class, and sphere intersection: 6.4

--- CPU_RT/RT1W/sphere.h
+++ CPU_RT/RT1W/sphere.h
@@ -26,22 +26,27 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 	auto c = oc.length_squared() - radius * radius;
 	auto discriminant = half_b * half_b - a * c;
 
-	if (discriminant > 0) {
+	if (discriminant > 0) 
+	{
 		auto root = sqrt(discriminant);
 
 		auto temp = (-half_b - root) / a;
-		if (temp < t_max && temp > t_min) {
+		if (temp < t_max && temp > t_min) 
+		{
 			rec.t = temp;
 			rec.p = r.at(rec.t);
-			rec.normal = (rec.p - center) / radius;
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
 			return true;
 		}
 
 		temp = (-half_b + root) / a;
-		if (temp < t_max && temp > t_min) {
+		if (temp < t_max && temp > t_min) 
+		{
 			rec.t = temp;
 			rec.p = r.at(rec.t);
-			rec.normal = (rec.p - center) / radius;
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
 			return true;
 		}
 	}
