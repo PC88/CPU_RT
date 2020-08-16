4185e4dcc3957cdafe803f65bd91f936d8fe2d2a
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-16 18:16:46 +0100

    sphere code updated to show all the 9.0 changes. added to the world as with materials, lambertion and metal. - 9.5

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -7,7 +7,8 @@
 #include "RT1W/hittable_list.h"
 #include "RT1W/camera.h"
 #include "RT1W/colour.h"
-#include "RT1W/material.h"
+#include "RT1W/metal.h"
+#include "RT1W/lambertian.h"
 
 /// This will be an evolving merge of my attempts to understand much of ray tracing by
 /// merging my own understanding, with that of Peter Shirley`s RT in one weekend series,
@@ -87,8 +88,16 @@ int main()
 
 	// World
 	hittable_list world;
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// Camera
 	camera cam;
