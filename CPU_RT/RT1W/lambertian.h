0894e3e4ba11d52d9def724c2ee111b8c66de954
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-16 18:05:26 +0100

    lambertian material added, and includes moved again, 9.3

---
+++ CPU_RT/RT1W/lambertian.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "RT1W/material.h"
+
+class lambertian : public material
+{
+public:
+	lambertian(const color& a) : albedo(a) {}
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override 
+	{
+		vec3 scatter_direction = rec.normal + random_unit_vector();
+		scattered = ray(rec.p, scatter_direction);
+		attenuation = albedo;
+		return true;
+	}
+
+public:
+	color albedo;
+};
+
--- CPU_RT/RT1W/rtweekend.h
+++ CPU_RT/RT1W/rtweekend.h
@@ -6,14 +6,17 @@
 #include <memory>
 
 
-// Usings
+// Common Headers
+#include "vec3.h"
+#include "ray.h"
 
+
+// Usings
 using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
 // Constants
-
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
@@ -43,7 +46,3 @@ inline double clamp(double x, double min, double max)
 	return x;
 }
 
-// Common Headers
-
-//#include "ray.h"
-//#include "vec3.h"
