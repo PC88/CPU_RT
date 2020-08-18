653b79a0b47d41b00ffeafec3106098b1850ed7a
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-18 17:20:22 +0100

    src updated to reflect the partial refraction changes

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -91,7 +91,7 @@ int main()
 	hittable_list world;
 
 	auto material_ground = make_shared<lambertian>(color(0.2, 0.2, 0.8));
-	auto material_center = make_shared<dielectric>(1.5);
+	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
 	auto material_left = make_shared<dielectric>(1.5);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
