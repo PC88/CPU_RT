6e1ff1e647f3cfbad369e9a31fa1d2d00b3b9f4e
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-18 17:12:06 +0100

    src modified to use dielectric material

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -9,6 +9,7 @@
 #include "RT1W/colour.h"
 #include "RT1W/metal.h"
 #include "RT1W/lambertian.h"
+#include "RT1W/dielectric.h"
 
 /// This will be an evolving merge of my attempts to understand much of ray tracing by
 /// merging my own understanding, with that of Peter Shirley`s RT in one weekend series,
@@ -90,8 +91,8 @@ int main()
 	hittable_list world;
 
 	auto material_ground = make_shared<lambertian>(color(0.2, 0.2, 0.8));
-	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_center = make_shared<dielectric>(1.5);
+	auto material_left = make_shared<dielectric>(1.5);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
