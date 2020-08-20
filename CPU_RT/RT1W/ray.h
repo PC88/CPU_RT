84d24dff8883da2fc8887db9a1007817811b194f
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-20 16:05:19 +0100

    ray and camera updates for motion blur added, moving_sphere class also added.

--- CPU_RT/RT1W/ray.h
+++ CPU_RT/RT1W/ray.h
@@ -4,8 +4,8 @@ class ray
 {
 public:
 	ray() {}
-	ray(const point3& origin, const vec3& direction)
-		: orig(origin), dir(direction)
+	ray(const point3& origin, const vec3& direction, double time = 0.0)
+		: orig(origin), dir(direction), tm(time)
 	{}
 
 	point3 origin() const { return orig; }
@@ -19,5 +19,6 @@ public:
 public:
 	point3 orig;
 	vec3 dir;
+	double tm;
 };
 
