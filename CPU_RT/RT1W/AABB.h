eb9d4467a4f7067cf457be0140444486ed704d77
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-25 17:02:23 +0100

    3.4 first iteration of AABB`s

---
+++ CPU_RT/RT1W/AABB.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "RT1W/rtweekend.h"
+
+class AABB
+{
+public:
+	aabb() {}
+	aabb(const point3& a, const point3& b) { _min = a; _max = b; }
+
+	point3 min() const { return _min; }
+	point3 max() const { return _max; }
+
+	bool hit(const ray& r, double tmin, double tmax) const 
+	{
+		for (int a = 0; a < 3; a++) 
+		{
+			auto t0 = fmin((_min[a] - r.origin()[a]) / r.direction()[a],
+				(_max[a] - r.origin()[a]) / r.direction()[a]);
+			auto t1 = fmax((_min[a] - r.origin()[a]) / r.direction()[a],
+				(_max[a] - r.origin()[a]) / r.direction()[a]);
+			tmin = fmax(t0, tmin);
+			tmax = fmin(t1, tmax);
+			if (tmax <= tmin)
+				return false;
+		}
+		return true;
+	}
+
+	point3 _min;
+	point3 _max;
+};
