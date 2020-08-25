4cf898205f449e5b71d0afbf185d86c1a9ab3272
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-25 17:12:42 +0100

    optimised inline AABB via the construction of Pixar and Kensler, 3.5

--- CPU_RT/RT1W/AABB.h
+++ CPU_RT/RT1W/AABB.h
@@ -10,16 +10,17 @@ public:
 	point3 min() const { return _min; }
 	point3 max() const { return _max; }
 
-	bool hit(const ray& r, double tmin, double tmax) const 
+	inline bool aabb::hit(const ray& r, double tmin, double tmax) const 
 	{
 		for (int a = 0; a < 3; a++) 
 		{
-			auto t0 = fmin((_min[a] - r.origin()[a]) / r.direction()[a],
-				(_max[a] - r.origin()[a]) / r.direction()[a]);
-			auto t1 = fmax((_min[a] - r.origin()[a]) / r.direction()[a],
-				(_max[a] - r.origin()[a]) / r.direction()[a]);
-			tmin = fmax(t0, tmin);
-			tmax = fmin(t1, tmax);
+			auto invD = 1.0f / r.direction()[a];
+			auto t0 = (min()[a] - r.origin()[a]) * invD;
+			auto t1 = (max()[a] - r.origin()[a]) * invD;
+			if (invD < 0.0f)
+				std::swap(t0, t1);
+			tmin = t0 > tmin ? t0 : tmin;
+			tmax = t1 < tmax ? t1 : tmax;
 			if (tmax <= tmin)
 				return false;
 		}
