d68f592f5a1bf2cd010a1904ea07441b4ee013cb
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-25 18:07:58 +0100

    BVH comparision functions added, 3.10

--- CPU_RT/RT1W/bvh.h
+++ CPU_RT/RT1W/bvh.h
@@ -20,6 +20,33 @@ public:
 
 	virtual bool bounding_box(double t0, double t1, AABB& output_box) const override;
 
+	inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) 
+	{
+		AABB box_a;
+		AABB box_b;
+
+		if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+			std::cerr << "No bounding box in bvh_node constructor.\n";
+
+		return box_a.min().e[axis] < box_b.min().e[axis];
+	}
+
+
+	bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) 
+	{
+		return box_compare(a, b, 0);
+	}
+
+	bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) 
+	{
+		return box_compare(a, b, 1);
+	}
+
+	bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) 
+	{
+		return box_compare(a, b, 2);
+	}
+
 public:
 	shared_ptr<hittable> left;
 	shared_ptr<hittable> right;
@@ -53,20 +80,25 @@ bvh_node::bvh_node( std::vector<shared_ptr<hittable>>& objects,
 
 	size_t object_span = end - start;
 
-	if (object_span == 1) {
+	if (object_span == 1) 
+	{
 		left = right = objects[start];
 	}
-	else if (object_span == 2) {
-		if (comparator(objects[start], objects[start + 1])) {
+	else if (object_span == 2) 
+	{
+		if (comparator(objects[start], objects[start + 1])) 
+		{
 			left = objects[start];
 			right = objects[start + 1];
 		}
-		else {
+		else 
+		{
 			left = objects[start + 1];
 			right = objects[start];
 		}
 	}
-	else {
+	else 
+	{
 		std::sort(objects.begin() + start, objects.begin() + end, comparator);
 
 		auto mid = start + object_span / 2;
