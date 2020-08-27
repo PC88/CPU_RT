6d310ef75aa882e830f76940c82533d6862b41dd
PC88 <32823980+PC88@users.noreply.github.com>
2020-08-27 15:37:48 +0100

    lambertian updated to use texture materials - 3.2

--- CPU_RT/RT1W/lambertian.h
+++ CPU_RT/RT1W/lambertian.h
@@ -1,22 +1,24 @@
 #pragma once
 #include "RT1W/material.h"
+#include "RT1W/texture.h"
 
 struct hit_record;
 
 class lambertian : public material
 {
 public:
-	lambertian(const color& a) : albedo(a) {}
+	lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+	lambertian(shared_ptr<texture> a) : albedo(a) {}
 
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override 
 	{
 		vec3 scatter_direction = rec.normal + random_unit_vector();
 		scattered = ray(rec.p, scatter_direction, r_in.time());
-		attenuation = albedo;
+		attenuation = albedo->value(rec.u, rec.v, rec.p);
 		return true;
 	}
 
 public:
-	color albedo;
+	shared_ptr<texture> albedo;
 };
 
