1a7535cba1e062c2dca6fe0902095738312c2263
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-10 15:15:15 +0100

    smoothed render of noise texture.

--- CPU_RT/RT1W/perlin.h
+++ CPU_RT/RT1W/perlin.h
@@ -1,5 +1,20 @@
 #pragma once
 
+// global for now
+inline double trilinear_interp(double c[2][2][2], double u, double v, double w)
+{
+	auto accum = 0.0;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			for (int k = 0; k < 2; k++)
+				accum += (i*u + (1 - i)*(1 - u))*
+				(j*v + (1 - j)*(1 - v))*
+				(k*w + (1 - k)*(1 - w))*c[i][j][k];
+
+	return accum;
+}
+
+
 class perlin 
 {
 public:
@@ -24,18 +39,6 @@ public:
 		delete[] perm_z;
 	}
 
-	inline double trilinear_interp(double c[2][2][2], double u, double v, double w) 
-	{
-		auto accum = 0.0;
-		for (int i = 0; i < 2; i++)
-			for (int j = 0; j < 2; j++)
-				for (int k = 0; k < 2; k++)
-					accum += (i*u + (1 - i)*(1 - u))*
-					(j*v + (1 - j)*(1 - v))*
-					(k*w + (1 - k)*(1 - w))*c[i][j][k];
-
-		return accum;
-	}
 
 	double noise(const point3& p) const 
 	{
@@ -57,7 +60,7 @@ public:
 							perm_z[(k + dk) & 255]
 					];
 
-		return trilinear_interp(c, u, v, w)
+		return trilinear_interp(c, u, v, w);
 	}
 
 private:
