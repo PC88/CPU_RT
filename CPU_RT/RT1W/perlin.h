f724a9dcd8abf6ca7537e1379483a96a08c80580
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-10 15:38:02 +0100

    turbulance function added: 5.6

--- CPU_RT/RT1W/perlin.h
+++ CPU_RT/RT1W/perlin.h
@@ -60,6 +60,21 @@ public:
 		return accum;
 	}
 
+	double turb(const point3& p, int depth = 7) const 
+	{
+		auto accum = 0.0;
+		auto temp_p = p;
+		auto weight = 1.0;
+
+		for (int i = 0; i < depth; i++) {
+			accum += weight * noise(temp_p);
+			weight *= 0.5;
+			temp_p *= 2;
+		}
+
+		return fabs(accum);
+	}
+
 	double noise(const point3& p) const 
 	{
 		auto u = p.x() - floor(p.x());
