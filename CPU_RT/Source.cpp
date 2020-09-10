3d375739ca4b737c95b9794fb865bd0a5680fb44
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-10 15:23:51 +0100

    scale valeu added, to tweak frequency, 5.4

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -152,8 +152,7 @@ hittable_list two_spheres()
 hittable_list two_perlin_spheres() 
 {
 	hittable_list objects;
-
-	auto pertext = make_shared<noise_texture>();
+	auto pertext = make_shared<noise_texture>(4);
 	objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
 	objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
