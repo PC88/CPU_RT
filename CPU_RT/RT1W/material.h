d9d4282915d906026060357a7b51476da99d5b2c
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-13 17:03:24 +0100

    emmisive material class added. 7.1

--- CPU_RT/RT1W/material.h
+++ CPU_RT/RT1W/material.h
@@ -4,6 +4,12 @@
 class material
 {
 public:
+	// not pure virtual: non enforced interface
+	virtual color emitted(double u, double v, const point3& p) const 
+	{
+		return color(0, 0, 0);
+	}
+
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 };
 
@@ -13,3 +19,25 @@ double schlick(double cosine, double ref_idx)
 	r0 = r0 * r0;
 	return r0 + (1 - r0)*pow((1 - cosine), 5);
 }
+
+class diffuse_light : public material 
+{
+public:
+	diffuse_light(shared_ptr<texture> a) : emit(a) {}
+	diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+	virtual bool scatter(
+		const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+	) const override 
+	{
+		return false;
+	}
+
+	virtual color emitted(double u, double v, const point3& p) const override 
+	{
+		return emit->value(u, v, p);
+	}
+
+public:
+	shared_ptr<texture> emit;
+};
