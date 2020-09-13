01069496380b34b37468ff20ebe0602001e4d4bd
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-13 17:17:07 +0100

    background colours added to ray colour func, code refactored to implement the earlier interface updates - 7.2

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -44,7 +44,7 @@ double hit_sphere(const point3& center, double radius, const ray& r)
 }
 
 // depth is added here to stop the recursions from blowing the stack
-color ray_color(const ray& r, const hittable& world, int depth)
+color ray_color(const ray& r, const color& background, const hittable& world, int depth)
 {
 	hit_record rec;
 
@@ -54,25 +54,22 @@ color ray_color(const ray& r, const hittable& world, int depth)
 		return color(0, 0, 0);
 	}
 
-	// the min value here being 0.001 fixes the subtle bug of shadow acne
-	if (world.hit(r, 0.001, infinity, rec)) 
+	// If the ray hits nothing, return the background color.
+	if (!world.hit(r, 0.001, infinity, rec))
 	{
-		// the replacement of "random_in_unit_sphere()" with "random_unit_vector()" makes the lighting simulate correct lambertian lighting
-		//point3 target = rec.p + rec.normal + random_unit_vector();
-
-		// the above commented code is the optimal code, this is just an example of how the first ray tracing papers did it below
-		//point3 target = rec.p + random_in_hemisphere(rec.normal);
-		//return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
-
-
-		ray scattered;
-		color attenuation;
-		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-		{
-			return attenuation * ray_color(scattered, world, depth - 1);
-		}
-		return color(0, 0, 0);
+		return background;
 	}
+
+	ray scattered;
+	color attenuation;
+	color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+	if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+	{
+		return emitted;
+	}
+
+	return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5*(unit_direction.y() + 1.0);
@@ -185,11 +182,13 @@ int main()
 	point3 lookat;
 	auto vfov = 40.0;
 	auto aperture = 0.0;
+	color background(0, 0, 0);
 
 	switch (4) 
 	{
 	case 1:
 		world = random_scene();
+		background = color(0.70, 0.80, 1.00);
 		lookfrom = point3(13, 2, 3);
 		lookat = point3(0, 0, 0);
 		vfov = 20.0;
@@ -199,21 +198,27 @@ int main()
 	default:
 	case 2:
 		world = two_spheres();
+		background = color(0.70, 0.80, 1.00);
 		lookfrom = point3(13, 2, 3);
 		lookat = point3(0, 0, 0);
 		vfov = 20.0;
 		break;
 	case 3:
 		world = two_perlin_spheres();
+		background = color(0.70, 0.80, 1.00);
 		lookfrom = point3(13, 2, 3);
 		lookat = point3(0, 0, 0);
 		vfov = 20.0;
 		break;
 	case 4:
 		world = earth();
+		background = color(0.70, 0.80, 1.00);
 		lookfrom = point3(13, 2, 3);
 		lookat = point3(0, 0, 0);
 		vfov = 20.0;
+		break;
+	case 5:
+		background = color(0.0, 0.0, 0.0);
 		break;
 	}
 
@@ -238,7 +243,7 @@ int main()
 				auto u = (i + random_double()) / (image_width - 1);
 				auto v = (j + random_double()) / (image_height - 1);
 				ray r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, world, max_depth);
+				pixel_color += ray_color(r, background, world, max_depth);
 			}
 			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
