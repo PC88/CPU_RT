88faf489fc5d7523b028de4f397e3ca3788ca8ed
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-13 18:40:10 +0100

    scene updated to render tranlation and rotation, cornell box standard render also added - 8.2

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -201,8 +201,15 @@ hittable_list cornell_box()
 	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 
 	// boxes
-	objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
-	objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
+	shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+	box1 = make_shared<rotate_y>(box1, 15);
+	box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+	objects.add(box1);
+
+	shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+	box2 = make_shared<rotate_y>(box2, -18);
+	box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+	objects.add(box2);
 
 	return objects;
 }
