c00fbe34d9992d34229487c5b221a15098f4947e
PC88 <32823980+PC88@users.noreply.github.com>
2020-09-25 23:18:25 +0100

    simple MC approach for x^2 -- 3.1

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -432,33 +432,15 @@ int main()
 	//	}
 	//}
 
-	int inside_circle = 0;
-	int inside_circle_stratified = 0;
-	int sqrt_N = 10000;
-	for (int i = 0; i < sqrt_N; i++) 
+	int N = 1000000;
+	auto sum = 0.0;
+	for (int i = 0; i < N; i++) 
 	{
-		for (int j = 0; j < sqrt_N; j++) 
-		{
-			auto x = random_double(-1, 1);
-			auto y = random_double(-1, 1);
-			if (x*x + y * y < 1)
-				inside_circle++;
-			x = 2 * ((i + random_double()) / sqrt_N) - 1;
-			y = 2 * ((j + random_double()) / sqrt_N) - 1;
-			if (x*x + y * y < 1)
-				inside_circle_stratified++;
-		}
+		auto x = random_double(0, 2);
+		sum += x * x;
 	}
-
-	auto N = static_cast<double>(sqrt_N) * sqrt_N;
 	std::cout << std::fixed << std::setprecision(12);
-	std::cout
-	<< "Regular    Estimate of Pi = "
-	<< 4 * double(inside_circle) / (sqrt_N*sqrt_N) << '\n'
-	<< "Stratified Estimate of Pi = "
-	<< 4 * double(inside_circle_stratified) / (sqrt_N*sqrt_N) << '\n';
-
-	std::cerr << "\nDone.\n";
+	std::cout << "I = " << 2 * sum / N << '\n';
 	
 	return 0;
 }
