e7832275df4eafa24bc2c5ebbfe61d6a1deaa4d7
PC88 <32823980+PC88@users.noreply.github.com>
2020-11-13 18:59:53 +0000

    7.14 - commit 2 - review

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -312,6 +312,19 @@ hittable_list final_scene()
 	return objects;
 }
 
+inline vec3 random_cosine_direction() 
+{
+	auto r1 = random_double();
+	auto r2 = random_double();
+	auto z = sqrt(1 - r2);
+
+	auto phi = 2 * pi*r1;
+	auto x = cos(phi)*sqrt(r2);
+	auto y = sin(phi)*sqrt(r2);
+
+	return vec3(x, y, z);
+}
+
 inline double pdf(const vec3& p) 
 {
 	return 1 / (4 * pi);
@@ -439,20 +452,17 @@ int main()
 	}*/
 
 	int N = 1000000;
+
 	auto sum = 0.0;
 	for (int i = 0; i < N; i++) 
 	{
-		auto r1 = random_double();
-		auto r2 = random_double();
-		auto x = cos(2 * pi*r1) * 2 * sqrt(r2*(1 - r2));
-		auto y = sin(2 * pi*r1) * 2 * sqrt(r2*(1 - r2));
-		auto z = 1 - r2;
-		sum += z * z*z / (1.0 / (2.0*pi));
+		auto v = random_cosine_direction();
+		sum += v.z()*v.z()*v.z() / (v.z() / pi);
 	}
+
 	std::cout << std::fixed << std::setprecision(12);
 	std::cout << "Pi/2     = " << pi / 2 << '\n';
 	std::cout << "Estimate = " << sum / N << '\n';
-
 	std::cin.get();
 
 
