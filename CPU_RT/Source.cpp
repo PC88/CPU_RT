3ac9ceb656b98a0f6b495f0bbb820bb9a8f71894
PC88 <32823980+PC88@users.noreply.github.com>
2020-11-13 16:57:19 +0000

    3.7 importance sampling - review. wrong constant changed was giving n/a index

--- CPU_RT/Source.cpp
+++ CPU_RT/Source.cpp
@@ -437,15 +437,15 @@ int main()
 	//	}
 	//}
 
-	int N = 1000000;
+	int N = 1;
 	auto sum = 0.0;
 	for (int i = 0; i < N; i++) 
 	{
-		auto x = pow(random_double(0, 8), 1. / 3.);
+		auto x = pow(random_double(0, 8), 1.0 / 3.0);
 		sum += x * x / pdf(x);
 	}
 	std::cout << std::fixed << std::setprecision(12);
 	std::cout << "I = " << 2 * sum / N << '\n';
-	
+	std::cin.get();
 	return 0;
 }
